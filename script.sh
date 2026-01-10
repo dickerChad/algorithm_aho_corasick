31698d4732b8a63fe5e7cfb5944d6441681507ff
DickerChad <souley4344@gmail.com>
2026-01-10 22:19:16 +0100

    Modif script

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
-# make
-# ./genere-texte 50000 3 > texte3.txt
-# ./genere-mots 10 5 25 3 > mots3.txt
-# ./ac-matrice mots3.txt texte3.txt > res-ac-matrice
-# ./ac-hachage mots3.txt texte3.txt > res-ac-hachage
-# diff res-ac-matrice res-ac-hachage
-
 #!/bin/bash
 
 # Script d'expérimentation pour le TP3 - Aho-Corasick
